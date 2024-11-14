fd88e6a19cac0451e39a01a02e10e9983c0f1861
Subhajit Sarkar <subhajitsarkar.dev@gmail.com>
2024-11-14 11:56:38 +0530

    import project images and projectdata file setup

---
+++ src/lib/projectData.ts
@@ -0,0 +1,26 @@
+export const project = [
+  {
+    title: "E-commerce Website",
+    description:
+      "A full-stack e-commerce website built with Next.js and Tailwind CSS.",
+    images: ["/bg.jpg"],
+  },
+  {
+    title: "E-commerce Website",
+    description:
+      "A full-stack e-commerce website built with Next.js and Tailwind CSS.",
+    images: ["/bg4.jpg", "/p.jpg"],
+  },
+  {
+    title: "E-commerce Website",
+    description:
+      "A full-stack e-commerce website built with Next.js and Tailwind CSS.",
+    images: ["/p.jpg", "/b1.png", "/bg4.jpg"],
+  },
+  {
+    title: "E-commerce Website",
+    description:
+      "A full-stack e-commerce website built with Next.js and Tailwind CSS.",
+    images: ["/p.jpg", "/b1.png", "/bg4.jpg"],
+  },
+];
